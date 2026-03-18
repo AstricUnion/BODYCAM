eb2ae75d11ffdb242653da65856c142ef610c9db
maxobur0001 <msblock09@gmail.com>
2026-03-18 17:33:00 +0300

    Blur and dlens in github

--- bodycam.lua
+++ bodycam.lua
@@ -18,8 +18,6 @@ if SERVER then
 else
     enableHud(nil, true)
     local current = OWNER:getEyeAngles()
-    local lastYaw = current.y
-    local slopeOnRotate = 0
     local slopeOnMove = 0
     local targetOffset = Angle()
     local currentOffset = Angle()
@@ -85,17 +83,16 @@ else
 
     render.createRenderTarget("vignette")
     render.createRenderTarget("dirtylens")
-    render.createRenderTarget("noise")
-    render.createRenderTarget("sharpen")
+    -- render.createRenderTarget("sharpen")
 
-    local sharpness = material.load("pp/sharpen")
-    local dirtylens = material.createFromImage("data/sf_filedata/dlens03.png", "")
+    -- local sharpness = material.load("pp/sharpen")
+    local dirtylens = material.create("gmodscreenspace")
+    dirtylens:setTextureURL("$basetexture", "https://raw.githubusercontent.com/AstricUnion/BODYCAM/refs/heads/main/dlens03.png")
     local screenspace = material.load("models/screenspace")
-    local noise = material.load("effects/security_noise2")
     if !screenspace then return end
     local fisheye = material.create("Refract_DX90")
-    -- fisheye:setTexture("$basetexture", "_rt_fullframefb")
-    fisheye:setTextureRenderTarget("$basetexture", "sharpen")
+    fisheye:setTexture("$basetexture", "_rt_fullframefb")
+    -- fisheye:setTextureRenderTarget("$basetexture", "sharpen")
     fisheye:setTexture("$dudvmap", "models/effects/fisheyelens_dudv")
     fisheye:setTexture("$normalmap", "models/effects/fisheyelens_normal")
     fisheye:setFloat("$refractamount", -0.07)
@@ -110,41 +107,42 @@ else
     local sw, sh
 
     local function renderTargets()
+        render.selectRenderTarget("dirtylens")
+        do
+            render.clear(Color(0, 0, 0, 0))
+            render.setMaterialEffectDownsample(fisheye, 0.3, 2)
+            render.setMaterialEffectBloom(fisheye, 1, 1, 1, 2)
+            render.drawTexturedRect(-200, -200, 1424, 1424)
+            render.drawBlurEffect(20, 20, 2)
+            render.setMaterialEffectSub(dirtylens)
+            render.drawTexturedRect(0, 0, 1024, 1024)
+        end
         render.selectRenderTarget("vignette")
         do
             render.clear(Color(0, 0, 0, 0))
+
+            -- Blur on corners
+            pushMask(function()
+                render.drawFilledCircle(512, 512, 250)
+            end)
+            render.setMaterial(fisheye)
+            render.drawTexturedRect(100, 100, 824, 824)
+            popMask()
+            render.drawBlurEffect(10, 10, 5)
+
+            -- Vignette
             pushMask(function()
                 render.drawFilledCircle(512, 512, 360)
             end)
             render.setColor(Color(0, 0, 0))
             render.drawRect(0, 0, 1024, 1024)
             popMask()
-            render.drawBlurEffect(5, 5, 10)
-        end
-        render.selectRenderTarget("dirtylens")
-        do
-            render.clear(Color(0, 0, 0, 0))
-            render.setMaterialEffectBloom(fisheye, 0.8, 0.8, 0.8, 4)
-            render.drawTexturedRect(-200, -200, 1424, 1424)
-            render.drawBlurEffect(20, 20, 2)
-            render.setMaterialEffectSub(dirtylens)
-            render.drawTexturedRect(0, 0, 1024, 1024)
-        end
-        render.selectRenderTarget("noise")
-        do
-            render.setMaterialEffectDownsample(noise, 0, 1.8)
-            render.drawTexturedRectUV(math.random(-2000, 0), math.random(-10, 0), 3024, 1034, 0, 0, 10, 10)
-            render.setMaterial()
-        end
-        render.selectRenderTarget("sharpen")
-        do
-            render.setMaterial(sharpness)
-            render.drawTexturedRect(0, 0, 1024, 1024)
+            render.drawBlurEffect(5, 5, 5)
         end
         render.selectRenderTarget()
     end
 
-    local holo = hologram.create(Vector(), Angle(), OWNER:getModel(), Vector(1.2, 1.2, 1.2))
+    local holo = hologram.create(Vector(), Angle(), OWNER:getModel(), Vector(1.1, 1.1, 1.1))
     if !holo then return end
     local function preprocess(side)
         holo:manipulateBoneScale(holo:lookupBone("ValveBiped.Bip01_" .. side .. "_Clavicle"), Vector(0))
@@ -161,7 +159,7 @@ else
         local pos = OWNER:getPos()
         holo:setClip(0, true, render.getEyePos(), Vector(0, 0, -1))
         local ang = OWNER:getAngles():setP(0):setR(0)
-        holo:setPos(pos - Vector(30, 3, 0):getRotated(ang))
+        holo:setPos(pos - Vector(27, 0, 0):getRotated(ang))
         holo:setAngles(ang)
         holo:setPose("move_x", (OWNER:getPose("move_x") * 2) - 1)
         holo:setPose("move_y", (OWNER:getPose("move_y") * 2) - 1)
@@ -180,13 +178,19 @@ else
         renderTargets()
     end)
 
+    local censorMat = material.create("UnlitGeneric")
     local function drawCensor()
+        local players = find.byClass("player")
         local npcs = find.byClass("npc_*")
+        table.add(players, npcs)
         local pos = render.getEyePos()
-        for _, v in ipairs(npcs) do
-            ---@cast v NPC
+        for _, v in ipairs(players) do
+            if v == OWNER then goto cont end
+            ---@cast v Entity
             local aPos = v:getBonePosition(v:lookupBone("ValveBiped.Bip01_Head1"))
             if !aPos then goto cont end
+            local tr = trace.line(pos, aPos, {OWNER, v}, MASK_SOLID)
+            if tr.Hit then goto cont end
             local ang = (aPos - pos):getAngle()
             local m = Matrix(ang + Angle(90, 0, 0), aPos - ang:getForward() * 10)
             m:rotate(Angle(0, -90, 0))
@@ -194,27 +198,32 @@ else
             do
                 render.enableDepth(false)
                 render.setColor(Color(0, 0, 0))
-                render.drawRect(-6, -6, 12, 12)
+                render.setMaterial(censorMat)
+                render.drawTexturedRect(-8, -8, 16, 16)
             end
             render.popMatrix()
             ::cont::
         end
     end
 
-    local lastEyeAng = Angle()
-
-    hook.add("PostDrawOpaqueRenderables", "", function()
+    hook.add("PostDrawTranslucentRenderables", "censor", function()
         drawCensor()
     end)
 
+
+    local lastEyeAng = Angle()
+
     hook.add("PostDrawHUD", "", function()
         local currentEyeAng = render.getAngles()
-        local diff = (lastEyeAng - currentEyeAng) * 4
+        local diffNotNorm = (lastEyeAng - currentEyeAng) * 4
+        local diff = Angle(
+            math.normalizeAngle(diffNotNorm.p),
+            math.normalizeAngle(diffNotNorm.y),
+            math.normalizeAngle(diffNotNorm.r)
+        )
         render.setMaterial(fisheye)
         render.drawTexturedRect(-diff.y - 200, -diff.p - 200, sw + 400, sh + 400)
         render.setMaterialEffectAdd("dirtylens")
-        render.drawTexturedRect(0, 0, sw, sh)
-        render.setMaterialEffectSub("noise")
         render.drawTexturedRect(0, 0, sw, sh)
         render.setRenderTargetTexture("vignette")
         render.drawTexturedRect(math.clamp(-diff.y, -50, 50) - 400, -diff.p - 350, sw + 800, sh + 700)
@@ -233,8 +242,28 @@ else
     end)
 
 
+    local lastOffset = Vector()
+    local function getViewmodelOffset(origin, angles)
+        local viewmodel = OWNER:getViewModel()
+        local viewmodelOffset = Vector()
+        local boneCount = viewmodel:getBoneCount()
+        for i=0, boneCount do
+            local mat = viewmodel:getBoneMatrix(i)
+            if !mat then goto cont end
+            local pos, ang = worldToLocal(mat:getTranslation(), mat:getAngles(), origin, angles)
+            viewmodelOffset = viewmodelOffset + pos + ang:setR(ang.r / 4) / 2
+            ::cont::
+        end
+        local offset = viewmodelOffset / boneCount / 3
+        ---@cast offset Vector
+        if offset:getLength() > 100 then return lastOffset end
+        lastOffset = math.lerpVector(0.2, lastOffset, offset)
+        return lastOffset
+    end
+
     ---@return ViewData
     hook.add("CalcView", "", function(origin, angles)
+        -- If player dead
         local deathRagdoll = OWNER:getDeathRagdoll()
         if deathRagdoll and isValid(deathRagdoll) then
             local pos, ang = deathRagdoll:getAttachment(3)
@@ -244,29 +273,17 @@ else
             walkAnimationFunc()
         end
         local velocity = OWNER:getLocalVelocity()
-        slopeOnRotate = slopeOnRotate - (angles.y - lastYaw)
-        slopeOnRotate = math.lerp(0.1, math.normalizeAngle(slopeOnRotate), 0) - ((slopeOnRotate / 60) * math.rand(-1, 1))
         slopeOnMove = math.lerp(0.1, slopeOnMove, velocity.x) - ((slopeOnMove / 22) * math.rand(-1, 1))
         velocityZ = math.lerp(0.1, velocityZ, velocity.z) - ((velocityZ / 20) * math.rand(-1, 1))
-        lastYaw = angles.y
         currentOffset = math.lerpAngle(0.0005, currentOffset, targetOffset)
         local slope = velocity.y / 40 - ((velocity.y / 500) * math.rand(-1, 1))
         local currentShake = ((angles - current) / 300 + Angle(0.03, 0.03, 0.03)) * Angle(math.rand(-1, 1), math.rand(-1, 1), math.rand(-1, 1))
-        local viewmodel = OWNER:getViewModel()
-        local viewmodelOffset = Angle()
-        local boneCount = viewmodel:getBoneCount()
-        for i=0, boneCount do
-            local mat = viewmodel:getBoneMatrix(i)
-            if !mat then goto cont end
-            local pos, ang = worldToLocal(mat:getTranslation(), mat:getAngles(), origin, angles)
-            viewmodelOffset = viewmodelOffset + pos + ang:setR(ang.r / 4) / 2
-            ::cont::
-        end
-        current = math.lerpAngle(0.2, current, angles + Angle(0, 0, 2 * -slope) + currentOffset - viewmodelOffset / boneCount / 5 + Angle(-5, 0, 0)) + currentShake
+        local vmOffset = getViewmodelOffset(origin, angles)
+        current = math.lerpAngle(0.2, current, angles + Angle(0, 0, 2 * -slope) + currentOffset - vmOffset) + currentShake
         ---@class obj: ViewData
         local obj = {
-            origin = origin + (angles:getRight() * 5) + Vector(0, 0, -2) + (angles:getForward() * 3),
-            angles = current + Angle(velocityZ / 50 + slopeOnMove / 30, 0, slopeOnRotate / 3) + walkAnimation + OWNER:getViewPunchAngles() * math.rand(-1, 1) / 2,
+            origin = origin + (angles:getRight() * 5) + Vector(0, 0, -2) + Vector(4, 0, 0):getRotated(Angle(0, angles.y, 0)) + (angles:getForward() * 5),
+            angles = current + Angle(velocityZ / 50 + slopeOnMove / 30, 0, 0) + walkAnimation + OWNER:getViewPunchAngles() * math.rand(-1, 1) / 2,
             fov = 140,
         }
         return obj
